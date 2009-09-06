8256abf40c641a075922d20aa625650c5a3feb2f
ed-s <ed-s@ea6537cf-e006-0410-91e3-b5ef31be2fb9>
2009-09-06 16:49:34 +0000

    git-svn-id: http://lib.modeldriven.org/MDLibrary/trunk/Applications/fUML-Reference-Implementation/trunk/@20058 ea6537cf-e006-0410-91e3-b5ef31be2fb9

---
+++ provision/src/fUML/Debug.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2008 Lockheed Martin Corporation.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Public License v1.0
+ * which accompanies this distribution, and is available at
+ * 
+ *
+ * Contributors:
+ *   MDS - initial API and implementation
+ *
+ */
+
+package fUML;
+
+import fUML.utility.MexSystem;
+import fUML.Debug;
+import UMLPrimitiveTypes.intList;
+
+/**
+ * <!-- begin-user-doc --> An implementation of the model object '
+ * <em><b>fUML::Debug</b></em>'. <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ * <li>{@link Debug#println <em>println</em>}</li>
+ * </ul>
+ * </p>
+ * 
+ * @generated
+ */
+
+public class Debug {
+
+    // Attributes
+
+    // Operations of the class
+    /**
+     * operation println <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @generated
+     */
+
+    public static void println(String message) {
+        MexSystem.println(message);
+
+    }
+
+} // Debug
