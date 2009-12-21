d943e25709b1c297e812af8fd3daa5f8c2daf41d
ed-s <ed-s@ea6537cf-e006-0410-91e3-b5ef31be2fb9>
2009-12-21 23:11:21 +0000

    git-svn-id: http://lib.modeldriven.org/MDLibrary/trunk/Applications/fUML-Reference-Implementation/trunk/@20404 ea6537cf-e006-0410-91e3-b5ef31be2fb9

---
+++ src/java/fUML/Debug.java
@@ -0,0 +1,41 @@
+
+/*
+ * Initial version copyright 2008 Lockheed Martin Corporation, except  
+ * as stated in the file entitled Licensing-Information. 
+ * 
+ * All modifications copyright 2009 Data Access Technologies, Inc.
+ *
+ * Licensed under the Academic Free License version 3.0 
+ * (http://www.opensource.org/licenses/afl-3.0.php), except as stated 
+ * in the file entitled Licensing-Information. 
+ *
+ * Contributors:
+ *   MDS - initial API and implementation
+ *
+ */
+
+package fUML;
+
+/**
+ * An implementation of the model object '
+ * <em><b>fUML::Debug</b></em>'.
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ * <li>{@link Debug#println <em>println</em>}</li>
+ * </ul>
+ * </p>
+ * 
+ */
+
+public class Debug {
+
+	/**
+	 * operation println
+	 * 
+	 */
+	public static void println(String message) {
+		System.out.println(message);
+	} // println
+
+} // Debug
